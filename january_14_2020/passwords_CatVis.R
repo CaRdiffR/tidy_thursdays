a5e161cd51809aafc3f11ba9d383a33f673bc876
Aura <auraf85@yahoo.it>
2020-01-16 19:03:50 +0000

    move png, changed script

--- january_14_2020/passwords_CatVis.R
+++ january_14_2020/passwords_CatVis.R
@@ -1,3 +1,21 @@
+# Get the Data
+
+passwords <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-14/passwords.csv')
+
+# Or read in with tidytuesdayR package (https://github.com/thebioengineer/tidytuesdayR)
+# PLEASE NOTE TO USE 2020 DATA YOU NEED TO UPDATE tidytuesdayR from GitHub
+
+# Either ISO-8601 date or year/week works!
+
+# Install via devtools::install_github("thebioengineer/tidytuesdayR")
+
+tuesdata <- tidytuesdayR::tt_load('2020-01-14') 
+tuesdata <- tidytuesdayR::tt_load(2020, week = 3)
+
+
+passwords <- tuesdata$passwords
+
+
 library(dplyr)
 category_counts<-passwords %>%
   filter(!is.na(category))%>%
