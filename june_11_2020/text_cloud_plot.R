e7db268ba951eff672f1ca91841adb9a86e83653
Dominik Krzeminski <dominik@appsilon.com>
2020-06-11 18:34:14 +0100

    updated cloud

--- june_11_2020/text_cloud_plot.R
+++ june_11_2020/text_cloud_plot.R
@@ -25,15 +25,19 @@ filtjobs <- filtjobs[-1]
 
 science$pos_x <- 0
 science$pos_y <- 0
-science$col   <- "black"
+science$job   <- "null"
 
+set.seed(0)
 allcolours <- 1:(length(filtjobs)+1) %>%
   map(function(x) colours()[sample(1:600)[[1]]])
+
+allcolours <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442",
+                "#0072B2", "#D55E00", "#CC79A7", "#c1fff5", "#ffa779")
 
 cnt <- length(grep(mainjob, categories))
 science[grepl(mainjob, categories), ]$pos_x <- 45*runif(cnt, -1, 1)
 science[grepl(mainjob, categories), ]$pos_y <- 45*runif(cnt, -1, 1)
-science[grepl(mainjob, categories), ]$col <- allcolours[[1]]
+science[grepl(mainjob, categories), ]$job <- mainjob
 
 
 ii <- 0
@@ -43,17 +47,21 @@ for (cat in filtjobs) {
   cnt <- length(grep(cat, categories))
   science[grepl(cat, categories), ]$pos_x <- r*cos(ii*step) + 30*runif(cnt, -1, 1)
   science[grepl(cat, categories), ]$pos_y <- r*sin(ii*step) + 30*runif(cnt, -1, 1)
-  science[grepl(cat, categories), ]$col <- allcolours[[ii+2]]
+  science[grepl(cat, categories), ]$job <- cat
   ii <- ii + 1
 }
 
-science <- science %>% filter(col != "black")
+science <- science %>% filter(job != "null")
 
-legend <- data.frame(col = unlist(allcolours), job  = c(mainjob, filtjobs))
+science$job <- as.factor(science$job)
 
-ggplot(science, aes(x = pos_x, y = pos_y, color = col, label = name)) +
+legend <- data.frame(col = unlist(allcolours),
+                     job  = c(mainjob, filtjobs))
+
+p <- ggplot(science, aes(x = pos_x, y = pos_y, color = col, label = name)) +
   geom_text() +
   theme_void() +
+  scale_colour_manual(values=unlist(allcolours)) +
   coord_cartesian(clip = "off") +
   theme(plot.background = element_rect(fill = 'grey11', colour = 'grey11'),
         legend.position="none",
@@ -72,4 +80,13 @@ ggplot(science, aes(x = pos_x, y = pos_y, color = col, label = name)) +
            hjust = 0,
            color = "white",
            family = "Andale Mono")
+
+leg_list <- legend %>% transpose() %>% map(function(x) {
+  annotate("text", x = -50,
+           y = -160,
+           label = x$job,
+           size = 10,
+           hjust = 0,
+           color = x$col)
+})
 
