150495c6f412b0213bc5c86649f01a58d200c264
Dominik Krzeminski <dkk33@cam.ac.uk>
2021-09-16 10:58:59 +0100

    minor fixes in time plot and song titles wordclouds

--- september_16_2021/billboard_d.R
+++ september_16_2021/billboard_d.R
@@ -2,6 +2,7 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(ggplot2)
 
 billboard <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-09-14/billboard.csv')
 
@@ -9,7 +10,8 @@ audio_features <- readr::read_csv('https://raw.githubusercontent.com/rfordatasci
 
 billboard$week_id <- as.Date(billboard$week_id, format="%m/%d/%Y")
 
-bsongperf <- billboard %>% group_by(song_id) %>% slice_min(peak_position, with_ties = FALSE)
+bsongperf <- billboard %>%
+  group_by(song_id) %>% slice_min(peak_position, with_ties = FALSE)
 
 system.time(
   x1 <- bsongperf %>%
@@ -31,9 +33,11 @@ system.time(
 
 audio_features <-audio_features[!duplicated(audio_features$song_id),]
 
-top_song_features <- bsongperf %>% left_join(audio_features, by = "song_id")
+top_song_features <- bsongperf %>%
+  left_join(audio_features, by = "song_id")
 
-features_by_year <- top_song_features %>% group_by(year=floor_date(week_id, "year")) %>%
+features_by_year <- top_song_features %>%
+  group_by(year=floor_date(week_id, "year")) %>%
   summarize(
     danceability=mean(danceability, na.rm=TRUE),
     energy=mean(energy, na.rm=TRUE),
@@ -52,4 +56,5 @@ ggplot(data = features_by_year, aes(year, value)) +
   labs(title = "TOP 100 BILLBOARD SONG FEATURES",
        y = "", x = "year") + 
   facet_wrap(. ~ feature, nrow=2, scales = "free") +
+  theme_minimal() +
   theme(legend.position = "none")
---
+++ september_16_2021/song_titles.R
@@ -0,0 +1,47 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+library(wordcloud)
+library(tm)
+
+billboard <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-09-14/billboard.csv')
+
+songs <- billboard %>%
+  group_by(decade = floor_date(week_id, years(x = 10))) %>% 
+  summarise(all_titles = paste(song, collapse = " "))
+
+songs$decade <- year(songs$decade)
+
+prep_text_for_plot <- function(df, index) {
+  text <- df[index,]$all_titles
+  docs <- Corpus(VectorSource(text))
+  options(warn=-1)
+  docs <- docs %>%
+    tm_map(removeNumbers) %>%
+    tm_map(removePunctuation) %>%
+    tm_map(stripWhitespace)
+  docs <- tm_map(docs, content_transformer(tolower))
+  docs <- tm_map(docs, removeWords, stopwords("english"))
+  options(warn=0)
+  
+  dtm <- TermDocumentMatrix(docs) 
+  matrix <- as.matrix(dtm) 
+  words <- sort(rowSums(matrix),decreasing=TRUE) 
+  df <- data.frame(word = names(words),freq=words)
+}
+
+plot_wordcloud <- function(df, title="") {
+  layout(matrix(c(1, 2), nrow=2), heights=c(1, 4))
+  par(mar=rep(0, 4))
+  plot.new()
+  text(x=0.5, y=0.5, title)
+  wordcloud(words = df$word, freq = df$freq, min.freq = 1,
+            max.words=100, random.order=FALSE, rot.per=0.8,
+            colors=brewer.pal(8, "Set1"))
+}
+
+decade_idx <- 7
+df <- prep_text_for_plot(songs, decade_idx)
+plot_wordcloud(df, paste("Year:", songs[decade_idx,]$decade))
